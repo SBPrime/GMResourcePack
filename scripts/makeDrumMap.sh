25ead1e7d86454c14430ab740a86a5c66f3d377d
Mowstyl <indikalesken@hotmail.com>
2019-09-09 21:46:45 +0200

    Updated scripts

--- scripts/makeDrumMap.sh
+++ scripts/makeDrumMap.sh
@@ -1,3 +1,6 @@
+echo "#ProgramID | Sound patch | Base volume"
+echo "D	block.note_block.basedrum			100%"
+echo "#Default mapping"
 for instrument in {1..127..1}
 do
     echo "$instrument	org.primesoft.gm.ds.i$instrument	100%"
